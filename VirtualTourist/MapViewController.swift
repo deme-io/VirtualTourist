bc1b2df65ad289db86fc3b721a0376538067ab67
Demetrius Henry <deme@deme.io>
2016-08-06 13:29:36 -0700

    Implement add pin to map with callout
    Implement add pin to map with callout

--- VirtualTourist/MapViewController.swift
+++ VirtualTourist/MapViewController.swift
@@ -11,9 +11,11 @@ import MapKit
 
 class MapViewController: UIViewController, MKMapViewDelegate {
     
+    // MARK: ===== Properties =====
     let userDefault = NSUserDefaults.standardUserDefaults()
     @IBOutlet weak var mapView: MKMapView!
     
+    // MARK: ===== View Methods =====
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,10 +28,27 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         mapRegion.center.longitude = userDefault.doubleForKey("mapViewCenterLong")
         
         mapView.setRegion(mapRegion, animated: true)
+        
+        let uilgr = UILongPressGestureRecognizer(target: self, action: #selector(MapViewController.addAnnotation(_:)))
+        uilgr.minimumPressDuration = 2.0
+        mapView.addGestureRecognizer(uilgr)
     }
     
-    
-   
+    // MARK: ===== Map Delegate Methods =====
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "pin"
+        var view: MKPinAnnotationView
+        if let dequeuedView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
+            as? MKPinAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.animatesDrop = true
+        }
+        return view
+    }
     
     func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
         
@@ -37,6 +56,38 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         userDefault.setDouble(mapView.region.span.longitudeDelta, forKey: "mapViewSpanLong")
         userDefault.setDouble(mapView.region.center.latitude, forKey: "mapViewCenterLat")
         userDefault.setDouble(mapView.region.center.longitude, forKey: "mapViewCenterLong")
+    }
+    
+    
+    func addAnnotation(gestureRecognizer:UIGestureRecognizer){
+        if gestureRecognizer.state == UIGestureRecognizerState.Began {
+            let touchPoint = gestureRecognizer.locationInView(mapView)
+            let newCoordinates = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
+            let annotation = MKPointAnnotation()
+            
+            annotation.coordinate = newCoordinates
+            
+            CLGeocoder().reverseGeocodeLocation(CLLocation(latitude: newCoordinates.latitude, longitude: newCoordinates.longitude), completionHandler: {(placemarks, error) -> Void in
+                if error != nil {
+                    print("Reverse geocoder failed with error" + error!.localizedDescription)
+                    return
+                }
+                
+                if let placemark = placemarks?[0] {
+                    annotation.title = placemark.name
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.mapView.addAnnotation(annotation)
+                        self.mapView.selectAnnotation(annotation, animated: true)
+                    })
+                } else {
+                    annotation.title = "Unknown Place"
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.mapView.addAnnotation(annotation)
+                        self.mapView.selectAnnotation(annotation, animated: true)
+                    })
+                }
+            })
+        }
     }
 
 
