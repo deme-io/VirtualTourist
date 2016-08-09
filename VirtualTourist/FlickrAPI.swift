157e7e3b83768e938321f79d09ba781fa20a1a19
Demetrius Henry <deme@deme.io>
2016-08-08 19:57:22 -0700

    Add FlickrAPI and FlickrAPIConstants Files
    Add FlickrAPI and FlickrAPIConstants Files

---
+++ VirtualTourist/FlickrAPI.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrAPI.swift
+//  VirtualTourist
+//
+//  Created by Demetrius Henry on 8/8/16.
+//  Copyright © 2016 Demetrius Henry. All rights reserved.
+//
+
+import UIKit
+
+class FlickrAPI: NSObject {
+    
+    
+}
---
+++ VirtualTourist/FlickrAPIConstants.swift
@@ -0,0 +1,62 @@
+//
+//  FlickrAPIConstants.swift
+//  VirtualTourist
+//
+//  Created by Demetrius Henry on 8/8/16.
+//  Copyright © 2016 Demetrius Henry. All rights reserved.
+//
+
+import UIKit
+
+extension FlickrAPI {
+    
+    // MARK: Flickr URL Keys
+    struct URL {
+        static let APIScheme = "https"
+        static let APIHost = "api.flickr.com"
+        static let APIPath = "/services/rest"
+    }
+    
+    // MARK: Flickr Parameter Keys
+    struct FlickrParameterKeys {
+        static let Method = "method"
+        static let APIKey = "api_key"
+        static let GalleryID = "gallery_id"
+        static let Extras = "extras"
+        static let Format = "format"
+        static let NoJSONCallback = "nojsoncallback"
+        static let SafeSearch = "safe_search"
+        static let Text = "text"
+        static let BoundingBox = "bbox"
+        static let Page = "page"
+    }
+    
+    // MARK: Flickr Parameter Values
+    struct FlickrParameterValues {
+        static let SearchMethod = "flickr.photos.search"
+        static let APIKey = "239c1afbb3bd9cb6f7fa731353f54daa"
+        static let ResponseFormat = "json"
+        static let DisableJSONCallback = "1" /* 1 means "yes" */
+        static let GalleryPhotosMethod = "flickr.galleries.getPhotos"
+        static let GalleryID = "5704-72157622566655097"
+        static let MediumURL = "url_m"
+        static let UseSafeSearch = "1"
+    }
+    
+    // MARK: Flickr Response Keys
+    struct FlickrResponseKeys {
+        static let Status = "stat"
+        static let Photos = "photos"
+        static let Photo = "photo"
+        static let Title = "title"
+        static let MediumURL = "url_m"
+        static let Pages = "pages"
+        static let Total = "total"
+    }
+    
+    // MARK: Flickr Response Values
+    struct FlickrResponseValues {
+        static let OKStatus = "ok"
+    }
+    
+}
